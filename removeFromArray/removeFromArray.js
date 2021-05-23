349dc5686e1f6069794a3f7b02f220360abb0e41
waylandsingh <wmsingh@ucdavis.edu>
2021-05-23 14:22:10 -0700

    tests passed to filter elements from arr

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,) {
+    
 
+    console.log(arguments)
+
+    // initalize the modified array the same as the input
+    let modifiedArray = arguments['0']
+
+    // iterate over the remaining arguments
+    for (let i=1; i<arguments.length; i++) {
+        //check for matches and 
+        modifiedArray = singleRemove(modifiedArray, arguments[i])
+        
+        //remove from array if dound
+    }
+
+    return modifiedArray
+
+    // remove a single element from an array
+    function singleRemove(arr, val) {
+        return arr.filter(element => {
+            return element !== val;
+        });
+
+    
 };
 
+}
+
+console.log(removeFromArray([1,2,3,4], 3, 2))
 module.exports = removeFromArray;
