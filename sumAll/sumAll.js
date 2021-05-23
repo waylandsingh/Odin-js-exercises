6548bd95b2f4707d625f90560f1caacbb8d8ceb1
waylandsingh <wmsingh@ucdavis.edu>
2021-05-23 15:56:56 -0700

    tests passed for sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,28 @@
-const sumAll = function() {
+const sumAll = function(n,m) {
+    // returns the sum of all numbers between n and m (inclusive)
+    // order doesn't matter
+
+    // return Error for negative numbers, non numbers
+    const negNumbers = (n < 0) || (m < 0)
+    const nonNumbers = nonInteger(n) || nonInteger(m)
+
+    if (negNumbers || nonNumbers) {
+        return 'ERROR'
+    }
+
+    // perform summation
+    let s = 0
+
+    for (let i=Math.min(n,m); i<=Math.max(n,m); i++) {
+        s += i
+    }
+
+    return s
+
+    // helper function to check if a value is NOT an integer
+    function nonInteger(val) {
+        return !(Number.isInteger(val))
+    }
 
 };
 
