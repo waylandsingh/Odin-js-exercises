6d99a665eefb27bf02ff7cdd5a0649f9aaaeb39d
waylandsingh <wmsingh@ucdavis.edu>
2021-05-23 14:03:10 -0700

    complete tests for hello, reverse functions

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n<0) {return 'ERROR'}
+    returnString = ''
+    for (let i=0; i<n; i++) {
+        returnString += str
+    }
+    return returnString
 };
 
 module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let returnStr = ''
 
+    // code here to reverse the string
+    // break the string into an array of letters
+    let letterArray = str.split('')
+
+    // iterate backwards through the array and append to returnStr
+    letterArray.reverse().forEach(element => {
+        returnStr += element
+    });
+
+    return returnStr
 };
+
+reverseString('hello');
+
 
 module.exports = reverseString;
