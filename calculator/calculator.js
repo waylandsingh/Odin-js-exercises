ea5d3befaa6b475fd7561a3113b57279e220cdb7
waylandsingh <wmsingh@ucdavis.edu>
2021-06-15 12:31:11 -0700

    add and subtract

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,9 +1,9 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
 const sum = function() {
