5c2eca6ac0b580f30f6a09ccd6a6b333f23b1b35
waylandsingh <wmsingh@ucdavis.edu>
2021-06-15 12:45:46 -0700

    memoized factorial

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,16 +10,26 @@ const sum = function(arr) {
 	return arr.reduce((acc,e)=>acc+e,0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  // use multiplication in the reduce callback
+  return arr.reduce((acc,e)=>acc*e, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	// compute the factorial of x
+  // use an array to store the results
+  let results = new Array()
+  results[0] = 1;
+
+  for (let i=1; i<=x; i++) {
+    results[i] = results[i-1] * i;
+  }
+  console.log(results)
+  return results[results.length - 1]
 };
 
 module.exports = {
