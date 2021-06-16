28e3d27093fd2533471dd9d2ea07ba0fe06e3c09
waylandsingh <wmsingh@ucdavis.edu>
2021-06-16 16:33:52 -0700

    use filter+regex to clean and check palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    // assuming clean input of a string here
+    // remove punctuation and spaces in the string
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+    const noPunctuation = s.replace(regex, '');
+    let cleanwords = noPunctuation.toUpperCase().split('').filter((el) => el!=' ')
 
+    // iterate to the middle of the string
+    for (let i=0; i < Math.ceil(cleanwords.length/2); i++){
+    //check if the 'mirror' element in the array matches
+
+    // if not, return false
+    if (cleanwords[i] !== cleanwords[cleanwords.length - i - 1]) {
+        return false
+        }
+    }
+    return true
 };
 
 module.exports = palindromes;
+
+tf = palindromes('racecar {{!')
+console.log(tf)
