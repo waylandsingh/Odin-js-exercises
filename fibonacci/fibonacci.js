72cb73cd3ec5d0436bfb7345340991d9dd27de6b
waylandsingh <wmsingh@ucdavis.edu>
2021-06-16 16:45:00 -0700

    memoized iteration for fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // check that the value can be repd as a positive number
+    let n = parseInt(num)
+
+    if (n < 0){
+        return "OOPS"
+    }
+
+    let fibs = [0,1];
+
+    for (let i = 2; i <= num; i++){
+        fibs[i] = fibs[i-1] + fibs[i-2]
+    }
+
+    return fibs[fibs.length-1]
 
 };
 
 module.exports = fibonacci;
+
