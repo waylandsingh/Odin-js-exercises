8528782013705e8df6c5665e504e4e3ed831113e
waylandsingh <wmsingh@ucdavis.edu>
2021-06-16 17:13:17 -0700

    map from book entries in array to array of titles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,4 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // books is an array of objects
+    // get a title from each book: mapping from 1 to 1
+
+    const titles = books.map(e=>e.title)
+    return titles
 
 };
 
